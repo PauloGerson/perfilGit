016e1347bd137b4facf9b66c470dcd9b6e1c3cb5
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2022-12-28 10:07:59 -0300

    remodelando

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,24 +4,28 @@ const teste = document.querySelector('.teste')
     const btnEnviar = document.querySelector('.btn');
 
     btnEnviar.addEventListener('click', function(){
-        const valorInput = inputUser.value;
-        user(valorInput)
-        respositorio(valorInput)
+       if(!inputUser.value) return;
+        user(inputUser.value)
+        respositorio(inputUser.value)
     })
 } 
 
-async function respositorio(user){
+let qtd = 0;
+ async function respositorio(user){
     const respoList = [];
+    
     const respo = await fetch(`https://api.github.com/users/${user}/repos`);
     const retornoRepo = await respo.json();
 
     respoList.push(retornoRepo)
-    console.log(respoList)
+    console.log(respoList.length)
      
     respoList.map(el => {
         for( let listRepo of el){
+
+            console.log(el.length)
             
-            const div = document.createElement('div')
+           /*  const div = document.createElement('div')
             const p1 = document.createElement('p');
             const p2 = document.createElement('p');
             const link = document.createElement('a');
@@ -34,28 +38,32 @@ async function respositorio(user){
             div.append(p1)
             div.append(p2)
             link.append(div)
-            teste.append(link)
+            teste.append(link) */
         }
     }) 
 
-}
+} 
 
 const user = async (user)=>{
-    //user = 'paulogerson';
+    //let user1 = 'PauloGerson';
     const apigit = await fetch(`https://api.github.com/users/${user}`);
     const retorno = await apigit.json();
     console.log(retorno)
     const img = document.querySelector('img');
     const profileName = document.querySelector('.profile-nome')
     const profileDesc = document.querySelector('.profile-descricao');
-    const follow = document.querySelector('.profile-follower')
+    const follow = document.querySelector('.followers')
+    const following= document.querySelector('.following')
+    const repos = document.querySelector('.repos')
+
     
     
     img.setAttribute('src', retorno.avatar_url)
     profileName.innerHTML = `${retorno.name} - ${retorno.login} `
     profileDesc.innerHTML = `${retorno.bio}`
-    follow.innerHTML = `${retorno.followers} followers - ${retorno.following} following`
-
+    follow.innerHTML = `${retorno.followers}`
+    following.innerHTML = ` ${retorno.following}`
+    repos.innerHTML = `${retorno.public_repos}`
 } 
 
 procurUser()
